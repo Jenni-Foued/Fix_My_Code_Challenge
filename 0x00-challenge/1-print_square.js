d24ffa5397fbc62f2e7cd4b8f515015d1d304347
Jenni-Foued <medfouedjenni@gmail.com>
2021-04-11 12:45:56 +0100

    Task 1: parseInt 10

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 /*
-  Print a square with the character #
-  The size of the square must be the first argument
-  of the program.
+    Print a square with the character #
+    The size of the square must be the first argument
+    of the program.
 */
+
 if (process.argv.length <= 2) {
   process.stderr.write('Missing argument\n');
   process.stderr.write('Usage: ./1-print_square.js <size>\n');
@@ -11,7 +12,7 @@ if (process.argv.length <= 2) {
   process.exit(1);
 }
 
-const size = parseInt(process.argv[2], 16);
+const size = parseInt(process.argv[2], 10);
 
 for (let i = 0; i < size; i++) {
   for (let j = 0; j < size; j++) {
